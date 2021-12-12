c7a872a8473afda6e8e2a3fdcfeb7a55e3ceeac1
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-12 16:51:51 +0300

    change name of the project in settings.py

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Projects, Votes, Comments, Profile
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponseRedirect, JsonResponse
 
 # Create your views here.
