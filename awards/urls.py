98fd2ccb27912829e7b8bdfef99115d594cebc3a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-12-13 15:32:03 +0300

    add css files so as to stop getting the 500 internal server error and also change debug to true

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,7 @@
 from os import name
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,4 +12,8 @@ urlpatterns = [
     path("project/<int:project_id>/", views.project_detail, name = "details"),
     path("search/projects/results/", views.search, name = "search"),
     path("logout/", views.logout, name = "logout"),
+    path("api/projects/", views.ProjectList.as_view()),
+    path("api/profile/", views.ProfileList.as_view()),
+    path("token/", obtain_auth_token),
+    path("developer/api/", views.apiView, name = "api"),
 ]
